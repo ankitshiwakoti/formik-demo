ef9de6618cb6383e4c6eeeeee1de30eda2af6c6c
ankit shiwakoti <shiwakotiankit@gmail.com>
2023-05-02 07:08:39 +0545

    formik practice done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Reyoutubeform from "./components/Reyoutubeform";
+// import Youtubeform from "../src/components/youtubeform";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Youtubeform /> */}
+      <Reyoutubeform />
     </div>
   );
 }
